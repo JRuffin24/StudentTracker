205e420cc4fb068fd3a286780e82774dcca0bac1
Jarrett Ruffin <jarrettruffin24@gmail.com>
2022-06-24 16:52:12 -0400

    Add project files.

---
+++ StudentTracker/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using StudentTracker.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StudentTracker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public  MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        async void AddTerm_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddTerm());
+        }
+
+       async void EditTerm_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EditTerm());
+        }
+    }
+}
---
+++ StudentTracker/Views/ClassDetails.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StudentTracker.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ClassDetails : ContentPage
+    {
+        public ClassDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
