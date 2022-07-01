bc4a0db1d0b07f72a9336ecab68de32509ef499b
Jarrett Ruffin <jarrettruffin24@gmail.com>
2022-07-01 16:32:50 -0400

    Add Assessment and Assessment Detail pages have been designed.

--- StudentTracker/Views/AddAssessment.xaml.cs
+++ StudentTracker/Views/AddAssessment.xaml.cs
@@ -16,5 +16,17 @@ namespace StudentTracker.Views
         {
             InitializeComponent();
         }
+        private void AddAssessmentButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void CancelButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void Notifications_OnToggle(object sender, EventArgs e)
+        {
+
+        }
     }
 }
