c16e4fd2fced6c47544f3e03029a193d1aa60577
Jarrett Ruffin <jarrettruffin24@gmail.com>
2022-07-28 16:48:26 -0400

    Changes to EditAssessment Page

--- StudentTracker/Views/EditAssessment.xaml.cs
+++ StudentTracker/Views/EditAssessment.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using StudentTracker.Models;
+using StudentTracker.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +21,32 @@ namespace StudentTracker.Views
         public EditAssessment(Tests test)
         {
             InitializeComponent();
+            ClassID.Text = test.ClassID.ToString();
+            AssessmentID.Text = test.AssessmentID.ToString();
+            ClassNameText.Text = test.ClassName;
+            Assessment1TypePicker.SelectedItem = test.AssessmentType;
+            TestStartDatePicker.Date = test.StartDate;
+            TestEndDatePicker.Date = test.EndDate;
+        }
 
+        async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            await DatabaseService.UpdateAssessment(Int32.Parse(AssessmentID.Text), Int32.Parse(ClassID.Text), ClassNameText.Text, Assessment1TypePicker.SelectedItem.ToString(), TestStartDatePicker.Date,
+                TestEndDatePicker.Date);
+
+            await Navigation.PopAsync();
+        }
+        async void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+            var id = int.Parse(AssessmentID.Text);
+
+            await DatabaseService.DeleteTest(id);
+
+            await Navigation.PopAsync();
+        }
+        async void CancelButton_Clicked (object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
         }
     }
 }
